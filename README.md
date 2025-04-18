90e8023611b0e5a1282091a60c7ab0855362c3df
pawanperla <perlapawan25@gmail.com>
2025-04-18 21:54:40 +0530

    Learnt the usage @Value

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SPRING_LEARNING
+
 This is a repo that tracks my spring learnings
+
+## mvnw files
+
+These files are deleted because it is not needed when working. Basically, these files are there in your directory to install latest maven version and to run the project. When you have maven installed locally on your computer it is not needed to add these files in the directory. Thats why deleted those two files.
--- SpringBoot_Learning/src/main/java/org/SpringBoot_Learning/Controller/FunRestController.java
+++ SpringBoot_Learning/src/main/java/org/SpringBoot_Learning/Controller/FunRestController.java
@@ -1,12 +1,20 @@
 package org.SpringBoot_Learning.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${developer.name}")
+    private String developerName;
+
+    @Value("${developer.email}")
+    private String developerEmail;
+
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World";
+        return developerName + " - " + developerEmail;
     }
 }
