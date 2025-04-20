a4ed9768d52b45b0fbf86d2de1675fc8c4870bdd
pawanperla <perlapawan25@gmail.com>
2025-04-20 22:06:40 +0530

    Entity class and DataSource configurations

---
+++ SpringBoot_Learning/src/main/java/org/SpringBoot_Learning/Entity/Student.java
@@ -0,0 +1,75 @@
+package org.SpringBoot_Learning.Entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String last_name, String email) {
+        this.firstName = firstName;
+        this.last_name = last_name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", firstName=" + firstName + ", last_name=" + last_name + ", email=" + email + "]";
+    }
+
+}
--- SpringBoot_Learning/src/main/java/org/SpringBoot_Learning/SpringBootLearningApplication.java
+++ SpringBoot_Learning/src/main/java/org/SpringBoot_Learning/SpringBootLearningApplication.java
@@ -2,6 +2,8 @@ package org.SpringBoot_Learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 // @SpringBootApplication(scanBasePackages = { "org.util", "org.SpringBoot_Learning" })
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class SpringBootLearningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 		System.out.println("Spring Boot Application Started");
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] aStrings) {
+		return runner -> System.out.println("CommandLineRunner executed");
 	}
 
 }
