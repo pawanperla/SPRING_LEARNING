ee53fc15cf91047c0a3631604a3b06eeca7735b7
pawanperla <perlapawan25@gmail.com>
2025-04-29 20:34:14 +0530

    New Employee_DAO project and created Entity with application configurations

---
+++ Employee_DAO/src/main/java/org/Employee_DAO/EmployeeDaoApplication.java
@@ -0,0 +1,13 @@
+package org.Employee_DAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDaoApplication.class, args);
+	}
+
+}
